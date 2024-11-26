77e2b3caf7d347dc7de8df69988e9bd94344ba3b
Affan <affanmurtaza3@gmail.com>
2024-11-26 11:24:45 +0500

    Add HTML Pages

--- src/main/java/com/cloud/Web/Application/WebApplication.java
+++ src/main/java/com/cloud/Web/Application/WebApplication.java
@@ -2,17 +2,22 @@ package com.cloud.Web.Application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class WebApplication {
 
+	@GetMapping("/")
+	public String homePage() {
+		return "index"; // Return the "index.html" page
+	}
+
 	@GetMapping("/hello")
-	public String helloWorld()
-	{
-		return "<h1>Hello World</h1>";
+	public String helloWorld() {
+		return "hello"; // Return the "hello.html" page
 	}
 
 	public static void main(String[] args) {
